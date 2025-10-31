20a77fc54071971bb84a3298258ccc3dd7a9f358
ywwb355 <ywwb355@nbcb.com>
2025-10-31 17:34:53 +0800

    init

--- src/app.config.ts
+++ src/app.config.ts
@@ -5,7 +5,7 @@ export default defineAppConfig({
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
+    navigationBarTitleText: '导航页',
     navigationBarTextStyle: 'black'
   }
 })
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,19 +1,51 @@
 <template>
-  <view class="index">
-    <text>{{ msg }}</text>
+  <view class="page">
+    <view class="title">我的导航页</view>
+
+    <view class="grid-box">
+      <at-grid
+        :data="list"
+        @click="go"
+      />
+    </view>
   </view>
 </template>
 
-<script>
+<script setup lang="ts">
 import { ref } from 'vue'
-import './index.scss'
+import { AtGrid } from 'taro-ui-vue3'
+import Taro from '@tarojs/taro'
 
-export default {
-  setup () {
-    const msg = ref('Hello world')
-    return {
-      msg
+const list = ref([
+  {
+    image: 'https://img.icons8.com/color/96/gitlab.png',
+    value: 'GitLab',
+    url: 'https://gitlab.example/'
+  },
+  {
+    image: 'https://img.icons8.com/color/96/jenkins.png',
+    value: 'Jenkins',
+    url: 'https://jenkins.example/'
+  },
+  {
+    image: 'https://img.icons8.com/color/96/swagger.png',
+    value: 'Swagger',
+    url: 'https://api.example/swagger'
+  }
+])
+
+const go = (item) => {
+  if (item.url) {
+    if (process.env.TARO_ENV === 'h5') {
+      window.open(item.url, '_blank')
+    }
+    if (process.env.TARO_ENV === 'weapp') {
+      Taro.showToast({ title: '小程序不可直接访问外链', icon: 'none' })
     }
   }
 }
 </script>
+
+<style lang="scss">
+
+</style>
